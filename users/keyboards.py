80a6b31083af34bf325d51d3cd0823ff65d28c50
Хаустов Александр Дмитриевич <khaustovad@trcont.ru>
2023-05-21 03:06:57 +0300

    add user keyboards

---
+++ users/keyboards.py
@@ -0,0 +1,9 @@
+from telegram import KeyboardButton, ReplyKeyboardMarkup
+
+
+def welcome_user_keyboard():
+    keyboard = [
+        [KeyboardButton("/start")]
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard)
+    return reply_markup
