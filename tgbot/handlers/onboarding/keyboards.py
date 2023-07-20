63f91c641a9f4a99af619c0217dde19ac27c3fb2
Хаустов Александр Дмитриевич <khaustovad@trcont.ru>
2023-07-20 20:53:16 +0300

    меню для юзеров/меню для админов

--- tgbot/handlers/onboarding/keyboards.py
+++ tgbot/handlers/onboarding/keyboards.py
@@ -8,25 +8,29 @@ from tgbot.handlers.onboarding.static_text import (
 from tgbot.states import ASK_QUESTION, QUESTION, EXPORT_QUESTIONS, SUPPORT_CHAT, BAN, END
 
 
-def make_keyboard_for_start_command() -> InlineKeyboardMarkup:
-    buttons = [
-        [
-            # InlineKeyboardButton(github_button_text, url="https://github.com/ohld/django-telegram-bot"),
-            # InlineKeyboardButton(secret_level_button_text, callback_data=f'{SECRET_LEVEL_BUTTON}'),
-            InlineKeyboardButton("Вопросы", callback_data=str(QUESTION)),
-            InlineKeyboardButton("Чат поддержки", callback_data=str(SUPPORT_CHAT)),
-        ],
-        [
-            InlineKeyboardButton(
-                "Получить вопросы", callback_data=str(EXPORT_QUESTIONS)
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                "Банхаммер", callback_data=str(BAN)
-            ),
-        ],
-        [InlineKeyboardButton("Закончить", callback_data=str(END))],
-    ]
+def make_keyboard_for_start_command(is_admin = False) -> InlineKeyboardMarkup:
+    if is_admin:
+        buttons = [
+            [
+                InlineKeyboardButton("Вопросы", callback_data=str(QUESTION)),
+                InlineKeyboardButton("Чат поддержки", callback_data=str(SUPPORT_CHAT)),
+            ],
+            [
+                InlineKeyboardButton(
+                    "Получить вопросы", callback_data=str(EXPORT_QUESTIONS)
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    "Банхаммер", callback_data=str(BAN)
+                ),
+            ],
+            [InlineKeyboardButton("Закончить", callback_data=str(END))],
+        ]
+    else:
+        buttons = [
+                [InlineKeyboardButton("Вопросы", callback_data=str(QUESTION))],
+                [InlineKeyboardButton("Закончить", callback_data=str(END))],
+            ]
 
     return InlineKeyboardMarkup(buttons)
